12f82d6e16541cf0e9115f23ebf146771445552b
Sean Phelan <seanphelan97@gmail.com>
2018-09-14 15:09:19 -0600

    add add get and post tests

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -2,7 +2,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, stopServer} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -14,19 +14,51 @@ describe('Recipes', function() {
   });
 
   after(function() {
-    return stopServer();
+    return closeServer();
   });
 
   it("should list recipes on GET", function() {
-
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body).length.to.be.greaterThan(1);
+      });
   });
 
-  it("should return a single item on GET with an id", function() {
-
-  });
+  // it("should return a single item on GET with an id", function() {
+  //   return chai.request(app).get('/recipes/1')
+  //     .then(res => {
+  //       expect(res).to.have.status(200);
+  //       expect(res.body).to.be.an('object');
+  //       expect(res.body).to.have.members(['id', 'name', 'ingredients']);
+  //     });
+  // });
   
   it("should create recipe on POST", function() {
-
+    const newItem = {
+      name: "coffee",
+      ingredients: [
+        "water", "coffee beans"
+      ]
+    };
+    return chai
+      .request(app)
+      .post("/recipes")
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.include.keys("id", "name", "ingredients");
+        expect(res.body.id).to.not.equal(null);
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, { id: res.body.id })
+        );
+      });
   });
 
   it("should update recipe on PUT", function() {
